86148bd7a667b51b4fdc8f612b8db4c53fef495f
setsuna <30497471+ksetsuna@users.noreply.github.com>
2017-07-28 13:13:43 +0800

    cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,49 +15,54 @@ const deploymentPath = configuration.path.deployment;
 const serverPath = configuration.path.server;
 
 const commonLoaders = [
-  {
-    test: /\.(js|jsx)$/,
-    loader: 'babel-loader',
-    query: {
-      presets: ['react', 'es2015']
-    }
-  },
-  {
-    test: /\.(less|css)$/,
-    use: ExtractTextPlugin.extract({
-      use:['css-loader', 'less-loader'],
-    }),
+    {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        query: {
+            presets: ['react', 'es2015']
+        }
+    },
+    {
+        test: /\.(less|css)$/,
+        use: ExtractTextPlugin.extract({
+            use: ['css-loader', 'less-loader']
+        }),
 
-    include: path.resolve(__dirname, 'source')
-  }
+        include: path.resolve(__dirname, 'source')
+    }
 ];
 
 const fileLoadersForClient = [
-  {
-    test: /\.(png|jpg|gif)$/,
-    loader: 'url-loader?limit=8192&name=/assets/[hash].[ext]'
-  },
-  {
-    test: /\.svg$/,
-    loader: 'file-loader?name=/assets/[hash].[ext]'
-  }
+    {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader',
+        query: {
+            limit: 8192,
+            name: '/assets/[hash].[ext]'
+        }
+    },
+    {
+        test: /\.svg$/,
+        loader: 'file-loader',
+        query: {name: '/assets/[hash].[ext]'}
+    }
 ];
 
 const fileLoadersForServer = fileLoadersForClient.map(i => {
-  let iCopy;
+    let iCopy;
 
-  if(!i.loader) return i;
-  
-  iCopy = JSON.parse(JSON.stringify(i));
-  iCopy.test = i.test;
-  if(iCopy.loader.split('?').length !== 2) iCopy.loader = iCopy.loader + '?';
-  iCopy.loader = iCopy.loader + '&emitFile=false';
-  return iCopy
+    if(!i.loader) return i;
+
+    iCopy = JSON.parse(JSON.stringify(i));
+    iCopy.test = i.test;
+    iCopy.query.emitFile = true;
+    return iCopy
 });
 
 
 const commonResolve = {
-  extensions: ['.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json']
 };
 
 
@@ -66,63 +71,50 @@ module.exports = [{
 
     entry: path.join(configuration.path.client, 'entry.jsx'),
     output: {
-      path: deploymentPath,
-      filename: 'assets/bundle.js',
+        path: deploymentPath,
+        filename: 'assets/bundle.js'
     },
 
     devtool: 'source-map',
 
     resolve: commonResolve,
     module: {
-      rules: commonLoaders.concat(fileLoadersForClient)
+        rules: commonLoaders.concat(fileLoadersForClient)
     },
     plugins: [
-      /*
-            new webpack.DefinePlugin({
-              'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            }),
-            new webpack.optimize.OccurrenceOrderPlugin(),
-            new webpack.optimize.UglifyJsPlugin({
-              compress: { warnings: false },
-              output: { comments: false },
-              mangle: true,
-              sourceMap: true,
-              beautify: false,
-              dead_code: true
-            })
-          */
-      ExtractCssConfig,
-    ],
-  },
 
-  {
-    name: 'server',
-
-    entry: path.join(configuration.path.server, 'server.jsx'),
-    output: {
-      path: deploymentPath,
-      filename: 'server.js',
-    },
-
-    target: 'node',
-    node: {
-      console: false,
-      global: false,
-      process: false,
-      Buffer: false,
-      __filename: false,
-      __dirname: false
-    },
-
-    externals: [nodeExternals()],
-
-    resolve: commonResolve,
-    module: {
-      rules: commonLoaders.concat(fileLoadersForServer)
-    },
-
-    plugins: [
-      ExtractCssConfig,
+        ExtractCssConfig
     ]
-  }
+},
+
+    {
+        name: 'server',
+
+        entry: path.join(configuration.path.server, 'server.jsx'),
+        output: {
+            path: deploymentPath,
+            filename: 'server.js'
+        },
+
+        target: 'node',
+        node: {
+            console: false,
+            global: false,
+            process: false,
+            Buffer: false,
+            __filename: false,
+            __dirname: false
+        },
+
+        externals: [nodeExternals()],
+
+        resolve: commonResolve,
+        module: {
+            rules: commonLoaders.concat(fileLoadersForServer)
+        },
+
+        plugins: [
+            ExtractCssConfig
+        ]
+    }
 ];
